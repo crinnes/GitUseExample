f212dd0b0d547947357c859e276bda0f5d05b5fa
Chris Innes <cinnes@Chriss-MacBook-Pro.local>
2017-09-02 22:27:28 +1000

    Adding map object

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Chris Innes on 2/9/17.
+//  Copyright © 2017 Chris Innes. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
